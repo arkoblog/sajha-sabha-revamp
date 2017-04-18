5bc5fd688ccd6b0af4a276ca5d629d816d6acffa
NikoruChen <shiland@163.com>
2017-04-18 12:24:48 -0400

    react suport for starter pack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import $ from 'jquery';
-import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-let counter = 0;
-setInterval(
-  () => {
-    counter += 1;
-    $('#main').html(`You've been on this page for ${counter} seconds.`);
-  }, 1000);
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
